9270d525f750bc2706cae03daed8a41f15ad4e6c
Rahul <Rahulyadav66618gmail.com>
2022-08-29 16:04:50 +0530

    commit

---
+++ MyFirstProjectApplication.java
@@ -0,0 +1,16 @@
+package com.Devops.MyFirstProject;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyFirstProjectApplication {
+
+	
+	public static void main(String[] args) {
+		System.out.println("Welcome Vishaal to Apmosys");
+		SpringApplication.run(MyFirstProjectApplication.class, args);
+	
+		
+	}
+
+}
